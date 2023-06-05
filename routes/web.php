0faac191d50861814ab15a32c4c6ebf5cd0cba83
ardhendu <ardhendu.sahoo@alumnetworks.com>
2023-06-05 18:04:05 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -307,7 +307,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 
 Route::group(['middleware' => 'auth'], function () {
-	Route::get('/', [MessagesController::class, 'index']);
+	Route::get('/connects', [MessagesController::class, 'index']);
 	Route::post('/idInfo', [MessagesController::class, 'idFetchData']);
     Route::post('/sendMessage', [MessagesController::class, 'send'])->name('send.message');
     Route::post('/fetchMessages', [MessagesController::class, 'fetch'])->name('fetch.messages');
